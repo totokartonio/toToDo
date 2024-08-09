b6c779067df77ba6a18bc5f84547b23d4f110718
Anton Efimchenko <126077080+totokartonio@users.noreply.github.com>
2024-08-09 22:18:46 +0200

    Rendering tasks (#7)
    * render-task-function
    
    * render-task-function-v1
    
    * render-task-function-v2
    
    * render-task-function-v3
    
    * render-task-function-v4
    
    ---------
    
    Co-authored-by: Anton Efimchenko <efimchenkoa@MacBook-Pro-Anton.local>

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let taskForm = document.getElementById('task-form');
+let listOfTasks = document.getElementById('task-list');
 let tasksList = [];
 
+//Хендлер для добавления id
 const createIncrementingIdGetter = () => {
   let id = 0;
   return function () {
@@ -10,25 +12,50 @@ const createIncrementingIdGetter = () => {
 
 const getTaskId = createIncrementingIdGetter();
 
+//Функция для добавления задачи на страницу
+const renderTask = task => {
+  const taskElement = document.createElement('div');
+  taskElement.classList.add('task');
+  taskElement.dataset.id = task.id;
+
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
+
+  const titleElement = document.createElement('p');
+  titleElement.textContent = task.title;
+
+  taskElement.append(checkbox, titleElement);
+
+  listOfTasks.append(taskElement);
+};
+
 taskForm.addEventListener('submit', function (event) {
   //Отменить стандарное действие кнопки и избежать обновления страницы
   event.preventDefault();
 
+  const form = event.target;
+
   //Собрать введеные в форму данные
-  const formData = new FormData(event.target);
+  const formData = new FormData(form);
 
   //Путем итерации записать данные в виде объекта с парами (ключ: значение,)
   const data = Object.fromEntries(formData.entries());
 
   //Добавить новую задачу в ссписок задач
-  tasksList.push({
+  let newTask = {
     id: getTaskId(),
     completed: false,
     ...data,
-  });
+  };
+
+  tasksList.push(newTask);
 
   //Обнулить значение в графе ввода
-  this.reset();
+  form.reset();
+
+  //Отобразить задачу на странице
+  renderTask(newTask);
 
   //Отобразить список задач в консоли
   console.log(tasksList);
